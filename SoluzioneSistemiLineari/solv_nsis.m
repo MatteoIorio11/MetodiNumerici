2df299161289308c83be04b8f0dc603a5a73ace6
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-05-24 09:05:55 +0200

    Update solv_nsis.m

--- SoluzioneSistemiLineari/solv_nsis.m
+++ SoluzioneSistemiLineari/solv_nsis.m
@@ -1,10 +1,10 @@
-function [inv] = solv_nsis(A, b)
+function [inve] = solv_nsis(A, b)
 [n, m] = size(b);
 [L, U, P, flag] = LUparziale(A);
 for j=1:m
     [x, f] = Lsolve(L, P*b(:, j));
     [y, f] = Usolve(U,x);
-    inv(:, j) = y;
+    inve(:, j) = y;
 end
 
 end

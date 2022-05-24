2a32a39306c1e4a5d0a34810d44438b72d824ceb
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-05-24 08:51:35 +0200

    Create InterpN.m

---
+++ InterpolazionePolinomiale/InterpN.m
@@ -0,0 +1,7 @@
+function [yv, a] = InterpN(x, y, xv)
+%Costruzione dei coefficienti a del polinomio interolatore di Newton
+
+a=diff_divise(x, y);
+%Valutazione il polinomio interpolatore di Newton nei punti del vettore xv
+%con o schmea Horner
+yv = pvalHornerN(a, x, xv);

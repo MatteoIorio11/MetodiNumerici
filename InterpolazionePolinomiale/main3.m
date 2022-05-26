0198ace0573c119c5ff304e552e165e3fda99ccb
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-05-26 10:54:34 +0200

    Create main3.m

--- InterpolazionePolinomiale/main3.m
+++ InterpolazionePolinomiale/main3.m
@@ -2,13 +2,17 @@ clc
 clear all
 close all
 
-xv = linspace(-1, 1, 22);
+xv = linspace(-1, 1, 200);
 f = @(x) sin(2*pi*x);
-xeq = linspace(-1, 1, 23);
-yeq = f(xeq);
-[pol_eq, coeff] = interpN(xeq, yeq, xv);
+xeq = linspace(-1, 1, 22);
 
-ys = yeq + ;
-[pol_s, coff] = interpN(xeq, ys, xv);
+yeq = f(xeq);
+ys = yeq + 0.0002 * randn(1, 22);
+
+[pol_eq] = InterpL(xeq, yeq, xv);
+[pol_s] = InterpL(xeq, ys, xv);
+
 figure(1)
-semilogy(xv, pol_eq, xv, ys);
+plot(xeq, yeq,'bo-',xeq, ys, 'r*', xv, pol_eq, 'b-', xv, pol_s, 'r-');
+errore = norm(yeq- ys, inf)/norm(yeq, inf);
+errore_ris = norm(pol_s - pol_eq, inf)/norm(pol_eq, 1);

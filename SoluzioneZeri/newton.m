ea91b216a88f8a663ea527ee198ad872108a8555
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-05-29 23:05:26 +0200

    Update newton.m

--- SoluzioneZeri/newton.m
+++ SoluzioneZeri/newton.m
@@ -1,31 +1,32 @@
-function [result, approssimazioni, iterations] = newton(fun,deri, x0, tolx, tolf, NMAX)
-m = deri(x0);
-if m == 0
-    disp('Errore')
-    result = [];
-    approssimazioni = [];
-    iterations = [];
+function [x1,xk,it]=newton(fname,fpname,x0,tolx,tolf,nmax)
+fx0=fname(x0);
+dfx0=fpname(x0);
+if abs(dfx0)>eps
+    x1 = x0 - fx0/dfx0;
+else
+    fprintf('Derivata nulla in x0 - EXIT \n')
+    x1 = [];
+    xk = [];
+    it = [];
     return
 end
-fx0 = fun(x0);
-result = x0 - fx0/m;
-fx = fun(result);
-iterations = 1;
-approssimazioni(iterations) = result;
-while iterations < NMAX && abs(fx) >= tolf && abs(fx0*m) > tolx*abs(result)
-    iterations = iterations + 1;
-    result = x0 - fx0/m;
-    approssimazioni(iterations) = result;
-    x0=result;
-    fx=fun(result);
-    fx0 = fx;
-    m = deri(x0);
-    if m == 0
-        disp('Errore')
-        result = [];
-        approssimazioni = [];
-        iterations = [];
-        return
+it = 1
+fx1 = fname(x1)
+xk(it) = x1;
+while it <= nmax && abs(fx1) > tolf && abs(m*fx0) > tolx*abs(x1)
+    it = it + 1
+    x0=x1;
+    fx0=fname(x0);
+    dfx0=fpname(x0);
+    if abs(dfx0)>eps
+         x1 = x0 - fx0/dfx0;
+    else
+        fprintf('Derivata nulla in xi - EXIT \n')
+        disp(dfx0)
+        break
     end
 end
+if it==nmax
+    fprintf('raggiunto massimo numero di iterazioni \n');
 end
+

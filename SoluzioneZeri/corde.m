96d5eff2a0f8a8ff684f3b0f8fde800bacb77539
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-05-29 23:02:03 +0200

    Update corde.m

--- SoluzioneZeri/corde.m
+++ SoluzioneZeri/corde.m
@@ -1,17 +1,23 @@
-function [result, approssimazioni, iterations] = corde(fun, m, x0, tolx, tolf, NMAX)
-if m == 0
-    disp('Error');
-    result = [];
-    approssimazioni = [];
-    iterations = [];
+function [x1,xk,it]=corde(fname,m,x0,tolx,tolf,nmax)
+
+fx0=fname(x0);
+%calcola soluzione x1
+x1 = x0 - x0/m;
+it=1;
+xk(it)=x1;
+fx1 = fname(x1)
+fprintf('it=%d  x=%8.15f \n',it,x1);
+while it<nmax && abs(fx1)>=tolf && abs(d)>=tolx*abs(x1)
+   x0=x1; 
+% Calcola soluzione x1
+   fx0 = fname(x0);
+   x1 = x0 - fx0/m;
+   fx1=fname(x1);
+   it=it+1;
+   xk(it,:)=x1; 
+   fprintf('it=%d  x=%8.15f \n',it,x1);
 end
-iterations = 1;
-result = x0 - fun(x0)/m;
-fx = fun(result);
-approssimazioni(iterations) = result;
-while iterations < NMAX && abs(fx) > tolf && abs(fun(x0)/m) > tolx*abs(result)
-    iterations = iterations + 1;
-    result = result - fx/m;
-    fx = fun(result);
-    approssimazioni(iterations)=result;
+if it==nmax 
+    fprintf('raggiunto massimo numero di iterazioni \n');
 end
+ 

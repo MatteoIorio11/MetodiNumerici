db592c8d6c35e7ba710aca2be46b8c3ba19fafc0
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-05-30 08:14:15 +0200

    Update bisezione.m

--- SoluzioneZeri/bisezione.m
+++ SoluzioneZeri/bisezione.m
@@ -2,23 +2,25 @@ function [x,xk,it]=bisez(fname,a,b,tol)
 
 fa=fname(a);
 fb=fname(b);
-if  sign(fb) * sign(fa) > 0
+if sign(fa)*sign(fb)>=0
    error('intervallo non corretto');
 else
-    maxit= ceil(log2((B-A)/tolx) -1)
+    maxit=ceil(log2((b-a)/tol)-1);
     %fprintf('n. di passi necessari=%d \n',maxit);
     it=0;
     while it<=maxit && abs(b-a)>=tol
         it=it+1;
-         %calcola xk(it) con la formula corrispondente
-        xk(it) = A + (B-A) * 0.5
+        xk(it)=a+(b-a)*0.5; 
+        %fprintf('it=%d  x=%8.15f \n',it,xk(it));
         fxk=fname(xk(it));
         if fxk==0
             break;
         elseif sign(fxk)*sign(fa)>0
-            B = xk(it)
+            a=xk(it);
+            fa=fxk;
         elseif sign(fxk)*sign(fb)>0
-            A = xk(it)
+            b=xk(it);
+            fb=fxk;
         end
     end
     x=xk(it);

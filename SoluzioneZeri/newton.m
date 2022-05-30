525a52ce0261409666bd0f14d2e1a11b2762a8e3
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-05-30 08:15:02 +0200

    Update newton.m

--- SoluzioneZeri/newton.m
+++ SoluzioneZeri/newton.m
@@ -1,25 +1,34 @@
 function [x1,xk,it]=newton(fname,fpname,x0,tolx,tolf,nmax)
+
 fx0=fname(x0);
 dfx0=fpname(x0);
 if abs(dfx0)>eps
-    x1 = x0 - fx0/dfx0;
+    d=fx0/dfx0;
+    x1=x0-d;
+    fx1=fname(x1);
+    it=1;
+    xk(it)=x1;
+    %fprintf('it=%d  x=%8.15f \n',it,x1);
 else
     fprintf('Derivata nulla in x0 - EXIT \n')
-    x1 = [];
-    xk = [];
-    it = [];
+    disp(dfx0)
+    x1=[];
+    xk=[];
+    it=[];
     return
 end
-it = 1
-fx1 = fname(x1)
-xk(it) = x1;
-while it <= nmax && abs(fx1) > tolf && abs(m*fx0) > tolx*abs(x1)
-    it = it + 1
+
+while it<nmax && abs(fx1)>=tolf && abs(d)>=tolx*abs(x1)
     x0=x1;
     fx0=fname(x0);
     dfx0=fpname(x0);
     if abs(dfx0)>eps
-         x1 = x0 - fx0/dfx0;
+        d=fx0/dfx0;
+        x1=x0-d;
+        fx1=feval(fname,x1);
+        it=it+1;
+        xk(it,:)=x1;
+        %fprintf('it=%d  x=%8.15f \n',it,x1);
     else
         fprintf('Derivata nulla in xi - EXIT \n')
         disp(dfx0)

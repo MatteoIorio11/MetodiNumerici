19f66623923312dc60ced6107b38ced6d1935e64
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-05-30 13:23:41 +0200

    Update InterpL.m

--- InterpolazionePolinomiale/InterpL.m
+++ InterpolazionePolinomiale/InterpL.m
@@ -1,22 +1,20 @@
-function yy = InterpL(x, f, xx)
+function yy = InterpL(x, y, xx)
 %
 %funzione che determina in un insieme di punti il valore del polinomio
 %interpolante ottenuto dalla formula di Lagrange.
 % DATI INPUT
 %  x  vettore con i nodi dell'interpolazione
-%  f  vettore con i valori dei nodi 
+%  y  vettore con i valori dei nodi 
 %  xx vettore con i punti in cui si vuole calcolare il polinomio
 % DATI OUTPUT
 %  y vettore contenente i valori assunti dal polinomio interpolante
 %
 
 n=length(x);
-m=length(xx);
-yy=zeros(1,m);
 for k=1:n
     p = plagr(x, k);
-    Lk = polyval(p, xx);
+    Lk(k, :) = polyval(p, xx);
     %calcola i coefficienti del k-esimo polinomio di Lagrange e denota con
     %Lk la sua valutazione nei punti xx 
-    yy=yy+f(k)*Lk;
 end
+yy = y * Lk;

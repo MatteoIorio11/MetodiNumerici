d5d9216e326c856066683e24b053cc9e51fb19e0
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-06-01 09:16:26 +0200

    Update simptoll.m

--- Integrazione/simptoll.m
+++ Integrazione/simptoll.m
@@ -6,6 +6,10 @@ IN = TrapComp(fun, a, b, N);
 while N<=Nmax && erro > tol
     N = 2*N;
     I2N = SimpComp(fun, a, b, N);
+    if abs(IN-I2N)==0
+        N=N/2;
+        return
+    end
     err = abs(IN-I2N)/15;
     IN = I2N;
 end

78c12e85a179a43ae70fae58931fc83d4ba24ac2
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-06-05 23:05:45 +0200

    Update Usolve.m

--- InterpolazionePolinomiale/Usolve.m
+++ InterpolazionePolinomiale/Usolve.m
@@ -6,6 +6,13 @@ if n ~= m
     flag = 1;
     return
 end
+% Test singolarita'
+if all(diag(U)) ~= 1
+	 disp('el. diag. nullo'), 
+	x=[]; 
+	flag=1; 
+	return
+ end
 y = zeros(n);
 for i=n:-1:1
     x(i) = (b(i) - U(i, i+1:n)*x(i+1:n))/U(i,i);

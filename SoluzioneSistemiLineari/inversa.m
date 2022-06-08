6bb95527600940a7016b9461ec4f4e851be9f38d
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-06-08 10:42:42 +0200

    Update inversa.m

--- SoluzioneSistemiLineari/inversa.m
+++ SoluzioneSistemiLineari/inversa.m
@@ -1,1 +1,9 @@
-
+function [inve] = inversa(L, U, b)
+% Ricordo che b è la matrice identità
+% Nel caso in cui si fosse usato LUpivot, bisogna moltiplicare b per la matrice P
+[n, m] = size(L);
+for i=1:n
+  [x, f] = Lsolve(L, b(:, i));
+  [y, f] = Usolve(U, x);
+  inve(:, i) = y;
+end

7bae90bf16569bf2f539855eebae10b77895d253
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-06-08 12:51:00 +0200

    Update secanti.m

--- SoluzioneZeri/secanti.m
+++ SoluzioneZeri/secanti.m
@@ -3,6 +3,7 @@ function [x1,xk,it]=secanti(fname,xm1,x0,tolx,tolf,nmax)
 fxm1=fname(xm1);
 fx0=fname(x0); 
 % m = (fxm1 - fx0)/(xm1 - x0)
+% d = fx0/m
 d=fx0*(x0-xm1)/(fx0-fxm1);
 x1=x0-d;
 fx1=feval(fname,x1); % identico a dire fx1 = fname(x1)

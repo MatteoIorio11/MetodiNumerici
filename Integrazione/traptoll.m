874bd7288bdac28b6732cad46ee1a5b69acb1d16
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-06-09 08:41:14 +0200

    Update traptoll.m

--- Integrazione/traptoll.m
+++ Integrazione/traptoll.m
@@ -6,6 +6,10 @@ IN = TrapComp(fun, a, b, N);
 while N<=Nmax && err > tol
     N = 2*N;
     I2N = TrapComp(fun, a, b, N);
+    if abs(IN-I2N)==0
+        N=N/2;
+        return
+    end
     err = abs(IN-I2N)/3;
     IN = I2N;
 end
